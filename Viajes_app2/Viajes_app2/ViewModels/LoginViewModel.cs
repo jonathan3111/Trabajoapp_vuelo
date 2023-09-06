62573aa103be9bb8c870ab4f2ab76cd89b78c030
jonathan3111 <jonathanmazaalfonso@gmail.com>
2023-09-05 20:24:02 -0500

    nuevo

---
+++ Viajes_app2/Viajes_app2/ViewModels/LoginViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Viajes_app2.Views;
+using Xamarin.Forms;
+
+namespace Viajes_app2.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private List<Usuario> usuarios; // Lista de usuarios
+
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+
+        public ICommand IniciarSesionCommand { get; }
+
+        public LoginViewModel()
+        {
+            IniciarSesionCommand = new Command(IniciarSesion);
+
+            // Inicializa la lista de usuarios con algunos ejemplos
+            usuarios = new List<Usuario>
+            {
+                new Usuario { Email = "jonathan@gmail.com", Contraseña = "123456" },
+                new Usuario { Email = "Dylan@gmail.com", Contraseña = "789456" },
+                // Agrega más usuarios según sea necesario
+            };
+        }
+
+        private void IniciarSesion()
+        {
+            bool inicioDeSesionExitoso = false;
+
+            foreach (var usuario in usuarios)
+            {
+                if (usuario.Email == Usuario && usuario.Contraseña == Contraseña)
+                {
+                    inicioDeSesionExitoso = true;
+                    break; // Se encontró una coincidencia, salir del bucle
+                }
+            }
+
+            if (inicioDeSesionExitoso)
+            {
+                // Inicio de sesión exitoso, navega a la página en cara
+                Application.Current.MainPage.Navigation.PushAsync(new Viajes_app2.Views.cara());
+            }
+            else
+            {
+                // Inicio de sesión fallido
+                MessagingCenter.Send(this, "LoginFailed");
+            }
+        }
+    }
+
+    public class Usuario
+    {
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
---
+++ Viajes_app2/Viajes_app2/Views/LoginPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Viajes_app2.ViewModels;
+
+namespace Viajes_app2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            MessagingCenter.Subscribe<LoginViewModel>(this, "LoginSucceeded", (sender) =>
+            {
+                // Inicio de sesión exitoso, puedes navegar a la página principal
+                // o realizar cualquier otra acción necesaria.
+                DisplayAlert("Éxito", "Inicio de sesión exitoso", "Aceptar");
+
+                // Limpia los campos de usuario y contraseña
+                ((LoginViewModel)this.BindingContext).Usuario = string.Empty;
+                ((LoginViewModel)this.BindingContext).Contraseña = string.Empty;
+            });
+
+            MessagingCenter.Subscribe<LoginViewModel>(this, "LoginFailed", (sender) =>
+            {
+                // Inicio de sesión fallido
+                DisplayAlert("Error", "Usuario o contraseña incorrectos", "Aceptar");
+            });
+        }
+
+    }
+}
